eeafe6efc76a58dae4d337a8628fb904eb26bba9
Sebastian Alonso <sebasalons101@gmail.com>
2017-01-08 12:50:36 +0100

    Initial Commit

---
+++ Prueba.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sebas
+ * Date: 1/8/17
+ * Time: 12:49 PM
+ */
+
+class Prueba
+{
+    public function name()
+    {
+        return "nombre";
+    }
+} 
